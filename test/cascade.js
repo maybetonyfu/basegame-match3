a897fa570d686169130546f8cda64fa319c67be4
tonysickpony <tonyfu.dev@gmail.com>
2016-01-12 00:36:20 +0000

    Test only on node 5.x

--- test/cascade.js
+++ test/cascade.js
@@ -3,8 +3,8 @@ import {cascade} from 'script/helper/cascade';
 
 test('Cascade should an array', (assert) => {
 
-  let list = [1,1,2,3,-1,1,-1,0]
-  
-  assert.deepEqual(cascade(list), [-1,-1,1,1,2,3,1,0])
-  assert.end();
+    let list = [1,1,2,3,-1,1,-1,0]
+
+    assert.deepEqual(cascade(list), [-1,-1,1,1,2,3,1,0])
+    assert.end();
 });
