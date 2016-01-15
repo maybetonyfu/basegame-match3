5f35bc25863d754b7d79f5fcba1e9dacee1912ad
tonysickpony <tonyfu.dev@gmail.com>
2016-01-15 01:02:33 +0000

    Change square thing to round

--- script/helper/cascade.js
+++ script/helper/cascade.js
@@ -2,11 +2,10 @@
 
 export let cascade = function (list) {
     let length = list.length
-    let after
     if( !length || length < 2 ) {
         console.error("Cannot cascade a non-array object or too short")
         return
     }
-    after = list.filter(value => value >= 0)
+    let after = list.filter(value => value >= 0)
     return [...Array(length - after.length).fill(-1), ...after]
 }
