8bd7683992a2f1332c323dfcaa6cbfcdec437f69
tonysickpony <tonyfu.dev@gmail.com>
2016-01-17 10:49:31 +0000

    Add css keyframe for tile manipulation

--- script/view/boardView.js
+++ script/view/boardView.js
@@ -64,41 +64,71 @@ let initiateBoardSpecs = (boardModel) => {
     let tileOuter = windowShorterSide / boardLonggerSide
     let boardHeight = tileOuter * boardModel.rows
     let boardWidth = tileOuter * boardModel.cols
-    let tileGutter = 1
+    let tileGutter = 5
     let tileInner = tileOuter - 2 * tileGutter
     let padding = tileInner / 2
     let style = document.createElement('style')
     style.type = 'text/css'
     style.innerHTML = `
-    .board {
-        height: ${boardHeight}px;
-        width: ${boardWidth}px;
+.board {
+    height: ${boardHeight}px;
+    width: ${boardWidth}px;
+}
+.row {
+    height: ${tileOuter}px;
+    width: ${boardWidth}px;
+}
+.tile {
+    width: ${tileInner}px;
+    height: ${tileInner}px;
+    line-height: ${tileInner}px;
+    margin: ${tileGutter}px;
+    font-size: ${padding}px;
+}
+@keyframes left-to-right {
+    0% {
+        transform: translateX(0px);
     }
-    .row {
-        height: ${tileOuter}px;
-        width: ${boardWidth}px;
+    100 % {
+        transform: translateX(60px);
     }
-    .tile {
-        width: ${tileInner}px;
-        height: ${tileInner}px;
-        line-height: ${tileInner}px;
-        margin: ${tileGutter}px;
-        font-size: ${padding}px;
+}
+@keyframes right-to-left {
+    0% {
+        transform: translateX(0px);
     }
-    `
+    100 % {
+        transform: translateX(-60px);
+    }
+}
+@keyframes top-to-bottom {
+    0% {
+        transform: translateY(0px);
+    }
+    100 % {
+        transform: translateY(60px);
+    }
+}
+@keyframes bottom-to-top {
+    0% {
+        transform: translateY(0px);
+    }
+    100 % {
+        transform: translateY(-60px);
+    }
+}
+`
     for (let row = 1; row < boardModel.rows; row ++) {
         let dropDistance = row * tileOuter
         let transitionDelay = row * 10
         let duration = 300
         style.innerHTML += `
-        .drop-${row} {
-            z-index: 1;
-            transition: transform ${duration}ms;
-            transition-delay: ${transitionDelay}ms;
-            transition-timing-function: cubic-bezier(.67,.21,.56,1);
-            transform: translateY(${dropDistance}px);
-        }
-        `
+.drop-${row} {
+    z-index: 1;
+    transition: transform ${duration}ms cubic-bezier(.67,.21,.56,1) ${transitionDelay}ms;
+    transform: translateY(${dropDistance}px);
+}
+`
     }
     document.getElementsByTagName('head')[0].appendChild(style)
 }
